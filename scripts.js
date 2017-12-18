ec1c994cdc5d57151620c1eeb9a64b70899e3d5f
tzits <tzitsman@gmail.com>
2017-12-18 16:04:37 -0500

    working out the logic

--- scripts.js
+++ scripts.js
@@ -1,4 +1,53 @@
-var array = ["Andrew","Ruby","Justin","Dezwon","Jacob","Ramar","Ellana","Clara","Jared","Dalton"]
+console.log('app.js loaded!')
+
+angular
+  .module('selector', [])
+  .controller("ListController", ListController)
+
+function ListController($scope) {
+	var vm = this;
+	vm.value = "asdf"
+	vm.include = true
+	console.log(vm.value)
+	vm.text = "How Many Teams Should There Be?"
+	vm.num = 2
+	$scope.pushed = function() {
+		vm.num = 
+		alert(vm.num)
+	}
+	$scope.setValue = function(val) {
+		vm.value = val
+		console.log(vm.value)
+		if (vm.value == "pairs") {
+			vm.text = "How Many People Per Group?"
+		} else {
+			vm.text = "How Many Teams Should there be?"
+		}
+	}
+	
+	$scope.setNum = function() {
+		console.log("num")
+	}
+	
+	$scope.generate = function() {
+		if (vm.value == "pairs") {
+			var x = 0
+			var name = ""
+				while (x < 2) {
+				var length = array.length
+				var i = Math.floor(Math.random() * length)
+				name = name + " " + array[i]
+				var length = length -1
+				array.splice(i, 1)
+				x ++
+			}
+		console.log(name)
+		}
+	}
+
+
+}
+
 function randomName() {
 	var x = 0
 	var name = ""
@@ -13,6 +62,3 @@ function randomName() {
 	console.log(name)
 }
 
-function pushed() {
-	alert("pushed")
-}
