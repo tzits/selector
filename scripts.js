3f29ebaa93ed78e6e07eb1ede9048113d3cc1e25
tzits <tzitsman@gmail.com>
2017-12-18 12:21:33 -0500

    adds basic logic

--- scripts.js
+++ scripts.js
@@ -1,1 +1,14 @@
-scripts.js
+var array = ["Andrew","Ruby","Justin","Dezwon","Jacob","Ramar","Ellana","Clara","Jared","Dalton"]
+function randomName() {
+	var x = 0
+	var name = ""
+	while (x < 2) {
+		var length = array.length
+		var i = Math.floor(Math.random() * length)
+		name = name + " " + array[i]
+		var length = length -1
+		array.splice(i, 1)
+		x ++
+	}
+	console.log(name)
+}
