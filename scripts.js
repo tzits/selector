a1ddaa0cb5b85234052e1300e94d895b704140b0
tzits <tzitsman@gmail.com>
2017-12-19 10:42:33 -0500

    logic for pairs

--- scripts.js
+++ scripts.js
@@ -29,7 +29,32 @@ function ListController($scope) {
 		var myArray = vm.array.split(",")
 		console.log(myArray)
 		if (vm.listType == "pairs") {
-			console.log('pairs')
+			console.log('pairs and ' + vm.num)
+			var myRealArray = []
+			while (myArray.length > 1) {
+				var x = 0
+				var name = ""
+				while(x < vm.num) {
+					var length = myArray.length
+					var i = Math.floor(Math.random() * length)
+					if (myArray[i]) {
+						name = name + " " + myArray[i]
+						var length = length -1
+						myArray.splice(i, 1)
+					}
+						if (x == vm.num - 1) {
+							myRealArray.push(name)
+						}
+					x ++
+				}
+			}
+			if (myArray.length != 0) {
+				console.log('avril')
+				for (var i = 0; i < myArray.length; i++) {
+					myRealArray[i] = myRealArray[i] + " " + myArray[i]
+				}
+			}
+			console.log(myRealArray)
 
 		} 
 		else {
